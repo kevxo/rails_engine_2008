6e6790211924e39082f9938ee90f1015351ef5dc
Kevin Cuadros <kev040798@gmail.com>
2020-12-15 19:12:26 -0500

    Incoprorate class method in models

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,4 +3,12 @@ class Item < ApplicationRecord
 
   has_many :invoice_items, dependent: :destroy
   has_many :invoices, through: :invoice_items
+
+  def self.find_item(attribute, value)
+    where("#{attribute} ILIKE ?", "%#{value}%").first
+  end
+
+  def self.find_all_items(attribute, value)
+    where("#{attribute} ILIKE ?", "%#{value}%")
+  end
 end
